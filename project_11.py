181f91ad80c5f60c0e13ea20fe09cdc7d8354899
Yuvaraja <panneer.yuvaraja321@gmail.com>
2020-07-03 13:31:36 +0100

    Removing unused code only
    There were some code commented out since they lacked functionality earlier.
    So removed those lines for clear and concise code.

--- project_11.py
+++ project_11.py
@@ -8,13 +8,6 @@ root = tk.Tk()
 root.title("Windows Adapter Toggler")
 root.geometry(WINDOW_SIZE)
 
-#to call a "disable bash file" from outside
-#tk.Button(root, text="DISABLE Internet", command=lambda: sub.call("C:/Users/DYUTRON/Desktop/disable_net.bat")).pack()
-#to call a "enable bash file" from outside
-#tk.Button(root, text="ENABLE Internet", command=lambda: sub.call("C:/Users/DYUTRON/Desktop/enable_net.bat")).pack()
-
-#to execute bash command right from python script
-# use "bashCommand=--cmd" for example to execute without buttons/tkinter
 tk.Button(root, text="DISABLE Internet", command=lambda: sub.call('netsh interface set interface name="Ethernet" admin=DISABLED')).pack()
 tk.Button(root, text="ENABLE Internet", command=lambda: sub.call('netsh interface set interface name="Ethernet" admin=ENABLED')).pack()
 os.system(bashCommand)
